014dc5bb40ffe24c54a8003ea02f5b1785941f97
Timothy Mugayi <django.course@gmail.com>
2023-03-16 20:48:57 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import pickle
+
+from google.auth.transport.requests import Request
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from llama_index import GPTSimpleVectorIndex, download_loader
+
+
+os.environ['OPENAI_API_KEY'] = 'SET-YOUR-OPEN-AI-API-KEY'
+
+
+def authorize_gdocs():
+    google_oauth2_scopes = [
+        "https://www.googleapis.com/auth/documents.readonly"
+    ]
+    with open("token.pickle", 'rb') as token:
+        cred = pickle.load(token)
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", google_oauth2_scopes)
+            cred = flow.run_local_server(port=0)
+        with open("token.pickle", 'wb') as token:
+            pickle.dump(cred, token)
+
+
+if __name__ == '__main__':
+
+    authorize_gdocs()
+    GoogleDocsReader = download_loader('GoogleDocsReader')
+    gdoc_ids = ['1ofZ96nWEZYCJsteRfqik_xNQTGFHtnc-7cYrf0dMPKQ']
+    loader = GoogleDocsReader()
+    documents = loader.load_data(document_ids=gdoc_ids)
+    index = GPTSimpleVectorIndex(documents)
+
+    while True:
+        prompt = input("Type prompt...")
+        response = index.query(prompt)
+        print(response)
